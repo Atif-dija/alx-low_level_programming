203b91314b45e94f01f90bdf36c043544449b0d5
Atif-dija <khadijaaif2@gmail.com>
2023-07-13 13:14:28 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,11 +7,11 @@
  * prints with puts fuction
  *
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
