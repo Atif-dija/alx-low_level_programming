5d67ea268555125c1a5c62c8c5e36d6c578284c4
Atif-dija <khadijaaif2@gmail.com>
2023-07-17 10:03:10 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,31 +10,28 @@
 
 int main(void)
 {
-	int num1 = 0;
+	int num1;
 	int num2;
 
-	while (num1 <= 99)
+	for (num1 = 0; num1 < 100; num1++)
 	{
-	num2 = num1;
-	while (num2 <= 99)
+	for (num2 = 0; num2 < 100; num2++)
 	{
-	if (num2 != num1)
+	if (num1 < num2)
 	{
 	putchar((num1 / 10) + 48);
 	putchar((num1 % 10) + 48);
 	putchar(' ');
 	putchar((num2 / 10) + 48);
-	putchar((num1 % 10) + 48);
+	putchar((num2 % 10) + 48);
 
-	if (num1 != 98 || num2 != 99)
+	if (!(num1 == 98 && num2 == 99))
 	{
 	putchar(',');
 	putchar(' ');
 	}
 	}
-	num2++;
 	}
-	num1++;
 	}
 	putchar('\n');
 	return (0);
