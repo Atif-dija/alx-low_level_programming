a47ce6097e7cda6d485d90cee39a00b4c19e6574
Atif-dija <khadijaaif2@gmail.com>
2023-07-18 19:20:49 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * adds two integers and returns the result
+ *
+ * @a: input of function
+ * @b: input pf function
+ *
+ * Return: a+b
+ */
+
+int add(int a, int b)
+{
+	int result;
+	result = a + b;
+	return (result);
+}
