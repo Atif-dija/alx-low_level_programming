224fe0de7de58b10410e210fe3932107030b33fc
Atif-dija <khadijaaif2@gmail.com>
2023-07-20 16:15:58 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * program that prints the numbers from 1 to 100
+ * for multiples of three print Fizz
+ * for the multiples of five print Buzz
+ * for numbers which are multiples of both three and five print FizzBuzz
+ *
+ * Return: Always 0;
+ */
+
+int main(void)
+{
+	int i;
+	char FB[] = "FizzBuzz";
+	char F[] = "Fizz";
+	char B[] = "Buzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s ", FB);
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s ", F);
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s ", B);
+		}
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
+
+
+
