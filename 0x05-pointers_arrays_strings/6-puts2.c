dd8786afa9fd01aae1ff629d72941b0c5ddda016
Atif-dija <khadijaaif2@gmail.com>
2023-07-24 14:02:01 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * puts2 - function that prints every other character of a string
@@ -8,11 +9,19 @@
  */
 void puts2(char *str)
 {
+	int length = 0;
+	char *s = str;
 
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
 	while (*str != '\0')
 	{
-			_putchar(*str);
-			str = str + 2;
+		_putchar(*str);
+		str += 2;
 	}
+
 	_putchar('\n');
 }
