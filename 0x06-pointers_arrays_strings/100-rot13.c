ee743632a2671e5533c746ff8b5c65710fb14293
Atif-dija <khadijaaif2@gmail.com>
2023-07-27 19:51:26 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 -  function that encodes a string using rot13
+ *
+ * @s: input of function
+ *
+ * Return:c
+ */
+
+char *rot13(char *s)
+{
+	char *c = s;
+	unsigned int i;
+	char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alph_13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFJHIJKLM";
+
+		while (*s)
+		{
+		for (i = 0; i < sizeof(alph); i++)
+
+		{
+			if (*s == alph[i])
+				*s = alph_13[i];
+		}
+		s++;
+	}
+	return (c);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
 int _strcmp(char *s1, char *s2);
 int _putchar(char ch);
 char *_strcat(char *dest, char *src);
