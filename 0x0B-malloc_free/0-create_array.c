08659c0fdb51d74e1b7009685a969a1e70c292c2
Atif-dija <khadijaaif2@gmail.com>
2023-08-08 09:28:01 -0700

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,11 +20,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0 || s == 0)
 		return (0);
 
-	while (size >= 0)
-	{
+	while (size--)
 		s[size] = c;
-		size--;
-	}
 	return (s);
 
 }
