cd850119961c58b47288c77753c7e8fd7b07e6d4
Atif-dija <khadijaaif2@gmail.com>
2023-08-10 12:44:10 -0700

    Trust no one

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];
 
-	for (j = 0; j < n && i < n + m; j++, i++)
+	for (j = 0; j < n; j++, i++)
 		s[i] = s2[j];
 
 	s[i] = '\0';
